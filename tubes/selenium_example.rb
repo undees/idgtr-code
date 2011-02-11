65f593af46fb466934559c5a67ac6537f484ad5b
Ian Dees <undees@gmail.com>
2011-02-11 13:01:35 -0800

    Update book title in Selenium example

--- tubes/selenium_example.rb
+++ tubes/selenium_example.rb
@@ -4,7 +4,7 @@ require 'selenium'
 
 browser = Selenium::SeleniumDriver.new \
   'localhost', 4444, '*firefox', 'http://www.pragprog.com', 10000 #<callout id="co.firefox"/>
-  
+
 browser.start
 browser.open 'http://www.pragprog.com'
 # END:launch
@@ -27,11 +27,11 @@ results = (1..num_results).map do |n|
   element  = "xpath=/descendant::td[@class='description'][#{n}]/h4/a" #<callout id="co.result_n"/>
   title    = browser.get_text(element)
   url      = browser.get_attribute(element + '@href') #<callout id="co.result_href"/>
-  
+
   {:title => title, :url => url, :element => element}
 end
 
-results.each do |r|  
+results.each do |r|
   puts 'Title: ' + r[:title]
   puts 'Link:  ' + r[:url]
   puts
@@ -40,7 +40,7 @@ end
 
 
 # START:details
-pickaxe = results.find {|r| r[:title].include? 'Programming Ruby 3'}
+pickaxe = results.find {|r| r[:title].include? 'Programming Ruby 1.9'}
 browser.click pickaxe[:element]
 browser.wait_for_page_to_load 5000
 # END:details
